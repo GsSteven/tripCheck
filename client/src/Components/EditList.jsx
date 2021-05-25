6707d4fd4a1b710b42b7c0f048a6aef4dd62a866
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-05-25 15:50:28 -0400

    added edit list component and css for front end

--- client/src/Components/EditList.jsx
+++ client/src/Components/EditList.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 
 export default function EditList({ name, items, refresh, close }) {
   const [changes, setChanges] = useState([]);
+  const [newItems, setNewItems] = useState([]);
+  const [addNewValue, setAddNewValue] = useState("");
 
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     const oldValue = e.target.defaultValue;
-    console.log(name, value, oldValue);
   };
 
   const displayEditInputs = () => {
@@ -25,12 +26,55 @@ export default function EditList({ name, items, refresh, close }) {
     return itemInputs;
   };
 
+  const displayNewInputs = () => {
+    const itemInputs = newItems.map((item) => {
+      return (
+        <input
+          type="text"
+          id={item}
+          name={item}
+          defaultValue={item}
+          onChange={handleChange}
+        />
+      );
+    });
+    return itemInputs;
+  };
+
+  const addToList = () => {
+    //add new item to array and reset input to blank
+    setNewItems((current) => [addNewValue, ...current]);
+    setAddNewValue("");
+  };
+
   return (
     <div className="editListWrapper">
       <h1>
         <u>{name}</u>
       </h1>
-      <ul className="itemsList">{displayEditInputs()}</ul>
+      <div className="addToEditContainer">
+        <input
+          type="text"
+          name="addToEdit"
+          value={addNewValue}
+          className="addToEdit"
+          onKeyPress={(e) => (e.code === "Enter" ? addToList() : "")}
+          onChange={(e) => setAddNewValue(e.target.value)}
+        />
+        <button
+          type="button"
+          className="addToEditButton"
+          title="Add to list"
+          onClick={addToList}
+        >
+          +
+        </button>
+      </div>
+      <ul className="newAddsList">{displayNewInputs()}</ul>
+      <ul className="editList">{displayEditInputs()}</ul>
+      <button type="button" className="saveEdit">
+        Save
+      </button>
     </div>
   );
 }
