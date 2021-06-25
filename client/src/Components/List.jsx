e23682bb78ee73faf52b53d54eaad7535b792c30
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-06-25 16:06:48 -0400

    added animated close button

--- client/src/Components/List.jsx
+++ client/src/Components/List.jsx
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 import ExpandedList from "./ExpandedList";
 import EditList from "./EditList";
+
+const inOutVariant = {
+  hidden: {
+    pathLength: 0,
+  },
+  visible: {
+    pathLength: 1,
+    transition: {
+      duration: 1.5,
+      ease: "easeInOut",
+    },
+  },
+};
 
 export default function List(props) {
   const [expandList, setExpandList] = useState(false);
@@ -29,14 +43,82 @@ export default function List(props) {
     }
   };
 
+  const closeButtonSVG = (
+    <motion.svg
+      onClick={toggleExpand}
+      className="closeButton"
+      viewBox="0 0 100 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      transition={{ staggerChildren: 1.5 }}
+    >
+      <motion.path
+        d="
+        M 0, 100
+        l 0, -100 
+        "
+        strokeWidth="20"
+        variants={inOutVariant}
+        initial="hidden"
+        animate="visible"
+      />
+      <motion.path
+        d="
+      M 0, 0
+      l 100, 0
+      "
+        strokeWidth="20"
+        variants={inOutVariant}
+        initial="hidden"
+        animate="visible"
+      />
+      <motion.path
+        d="
+      M 100, 0
+      l 0, 100
+      "
+        strokeWidth="20"
+        variants={inOutVariant}
+        initial="hidden"
+        animate="visible"
+      />
+      <motion.path
+        d="
+      M 100, 100
+      l -100, 0
+      "
+        strokeWidth="20"
+        variants={inOutVariant}
+        initial="hidden"
+        animate="visible"
+      />
+      <motion.path
+        d="
+      M 25, 25
+      l 50, 50
+      "
+        strokeWidth="10"
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: 1, transition: { duration: 1, delay: 0.75 } }}
+      />
+      <motion.path
+        d="
+      M 75, 25
+      l -50, 50
+      "
+        strokeWidth="10"
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: 1, transition: { duration: 1, delay: 1.25 } }}
+      />
+    </motion.svg>
+  );
+
   return (
     <div className="listWrapper">
       <h2 onClick={toggleExpand}>{props.name}</h2>
       {expandList && (
         <div className="expandedContainer">
-          <button className="closeButton" onClick={toggleExpand}>
-            X
-          </button>
+          {closeButtonSVG}
           <ExpandedList {...props} close={toggleExpand} openEdit={toggleEdit} />
         </div>
       )}
